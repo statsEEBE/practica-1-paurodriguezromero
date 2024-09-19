e7843e85b79d759ec73488aacdb74d9e63c73fa6
pau.rodriguez.romero <pau.rodriguez.romero@auleseebe.upc.edu>
2024-09-19 11:52:19 +0200

    ...

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,9 @@
 #Codigo para problema 2
+
+rm(list=ls())
+data=mtcars
+mpg<-data$mpg
+hist(data$mpg)
+pie(table(mpg))
+    
+    
--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,6 @@
 #Codigo para problema 3
+rm(list=ls())
+x<-1:30
+x
+sum(x)
+
